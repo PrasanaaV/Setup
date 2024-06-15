d2bc78c9001a26a25858dca8392c14ba99570f32
EdouardY <yuedouard@gmail.com>
2024-06-15 19:33:06 +0200

    spark

--- jobs/processing.py
+++ jobs/processing.py
@@ -1,76 +1,120 @@
-from pyspark.sql import SparkSession
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, lower, regexp_replace, split, when
-import json
-
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, lower, regexp_replace, split, array_contains
+from elasticsearch import Elasticsearch
+from pyspark.sql import SparkSession, Row
+from pyspark.sql.functions import col, lit, when
+from pyspark.sql.types import StringType
+import pandas as pd
+from elasticsearch.helpers import scan
+from confluent_kafka import Consumer, KafkaException, KafkaError
 import json
 
 # Créer une session Spark
 spark = SparkSession.builder \
     .appName("Word Count") \
     .master("spark://spark-master:7077") \
-    .config("spark.es.nodes", "676d19be3cd5480d980f3eb5dca1dede.us-central1.gcp.cloud.es.io") \
-    .config("spark.es.port", "9243") \
-    .config("spark.es.nodes.wan.only", "true") \
     .getOrCreate()
 
-# Lecture du fichier JSON pour le mapping
-with open('../files/mapping.json', 'r') as f:
-    line_mapping = json.load(f)
+# API keys for each index
+api_key_metro_station = "TWxtbTdZOEItODFDdXJHT2ptcXI6VkdHWl9kcF9UZmk3ZS05dldMVVV4Zw=="
+api_key_perimeter = "QzFsdkc1QUItODFDdXJHT1FuR2g6ZDNxbWdHVXVSQmFzZlZyYW5qSEw4UQ=="
+api_key_emplacement_stations_idf = "bkZWbjU0OEJKR3c1U0ExQWJMQXc6RVRESFRDOC1UMTJNMm5lc19CMXFnQQ=="
 
-# Création d'un DataFrame pour le mapping
-mapping_df = spark.createDataFrame(
-    [(k, v) for k, v in line_mapping.items()],
-    ["original", "mapped"]
-)
+es = Elasticsearch(
+        "https://7f0ef8badf50482b9b0d93ef141e14ec.us-central1.gcp.cloud.es.io:443",
+        api_key=api_key_perimeter
+    )
 
-# Lecture des index Elasticsearch
-metro_station_df = spark.read.format("es").load("metro_station")
-perimetre_df = spark.read.format("es").load("perimetre")
-emplacement_stations_idf_df = spark.read.format("es").load("emplacement-stations-idf")
+query = {
+    "query": {
+        "match_all": {}
+    },
+    "size": 1000
+}
+res = es.search(index="perimeter", body=query)
+data = [hit['_source'] for hit in res['hits']['hits']]
+df_pd = pd.DataFrame(data)
+perimeter_df = spark.createDataFrame(df_pd)
 
-# Normalisation des noms de station et lignes de métro
-metro_station_df = metro_station_df.withColumn("nom", lower(col("nom")))
-metro_station_df = metro_station_df.withColumn("nom", regexp_replace(col("nom"), "-", " "))
-metro_station_df = metro_station_df.withColumn("nom_split", split(col("nom"), " "))
-metro_station_df = metro_station_df.withColumn("number", regexp_replace(col("number"), "ligne", ""))
+def elasticsearch_query(es_client, index, query, field):
+    body = {
+        "query": {
+            "simple_query_string": {
+                "query": query,
+                "fields": [field]
+            }
+        }
+    }
+    results = scan(es_client, index=index, query=body)
+    return [hit["_source"] for hit in results]
 
-perimetre_df = perimetre_df.withColumn("ns3_stopname", lower(col("ns3_stopname")))
-perimetre_df = perimetre_df.withColumn("ns3_stopname", regexp_replace(col("ns3_stopname"), "-", " "))
-perimetre_df = perimetre_df.withColumn("ns3_stopname_split", split(col("ns3_stopname"), " "))
-perimetre_df = perimetre_df.withColumn("line", regexp_replace(col("line"), "STIF:Line::C", ""))
+es = Elasticsearch(
+        "https://7f0ef8badf50482b9b0d93ef141e14ec.us-central1.gcp.cloud.es.io:443",
+        api_key=api_key_metro_station
+    )
 
-emplacement_stations_idf_df = emplacement_stations_idf_df.withColumn("nom_gares", lower(col("nom_gares")))
-emplacement_stations_idf_df = emplacement_stations_idf_df.withColumn("nom_gares", regexp_replace(col("nom_gares"), "-", " "))
-emplacement_stations_idf_df = emplacement_stations_idf_df.withColumn("nom_gares_split", split(col("nom_gares"), " "))
-emplacement_stations_idf_df = emplacement_stations_idf_df.withColumn("res_com", regexp_replace(col("res_com"), "METRO ", ""))
-emplacement_stations_idf_df = emplacement_stations_idf_df.withColumn("res_com", regexp_replace(col("res_com"), "RER ", ""))
+# Extraction des noms d'arrêts de perimeter_df
+stopnames_list = perimeter_df.select('ns3_stopname').rdd.flatMap(lambda x: x).collect()
 
-# Mapping des lignes de métro dans perimetre
-perimetre_df = perimetre_df.join(mapping_df, perimetre_df.line == mapping_df.mapped, "left").drop("mapped").withColumnRenamed("original", "line_mapped")
-metro_station_df = metro_station_df.join(mapping_df, metro_station_df.number == mapping_df.mapped, "left").drop("mapped").withColumnRenamed("original", "number_mapped")
-emplacement_stations_idf_df = emplacement_stations_idf_df.join(mapping_df, emplacement_stations_idf_df.res_com == mapping_df.mapped, "left").drop("mapped").withColumnRenamed("original", "res_com_mapped")
+# Ajouter les colonnes supplémentaires au DataFrame
+for col_name in ["maps", "longitude", "latitude"] + [str(i) for i in range(1, 10)]:
+    perimeter_df = perimeter_df.withColumn(col_name, lit(None).cast(StringType()))
 
-# Fonction pour vérifier si tous les éléments de la plus petite liste sont contenus dans la plus grande liste
-def list_contains_all(smaller, larger):
-    return array_contains(larger, smaller)
+# Interrogation d'Elasticsearch et mise à jour de perimeter_df
+for element in stopnames_list[:50]:
+    try:
+        metro_station_output = elasticsearch_query(es, "metro_station", element, "nom")[0]
+        maps_value = metro_station_output.get("maps")
+        additional_columns = {str(i): metro_station_output.get(str(i)) for i in range(1, 10)}
 
-# Jointure des datasets sur les noms de station et lignes de métro
-merged_df = metro_station_df \
-    .join(perimetre_df, (list_contains_all(metro_station_df.nom_split, perimetre_df.ns3_stopname_split)) & (metro_station_df.number_mapped == perimetre_df.line_mapped)) \
-    .join(emplacement_stations_idf_df, (list_contains_all(metro_station_df.nom_split, emplacement_stations_idf_df.nom_gares_split)) & (metro_station_df.number_mapped == emplacement_stations_idf_df.res_com_mapped))
+        # Mise à jour du DataFrame
+        perimeter_df = perimeter_df.withColumn("maps", 
+                                               when(col("ns3_stopname") == element, lit(maps_value)).otherwise(col("maps")))
+        for col_name, col_value in additional_columns.items():
+            perimeter_df = perimeter_df.withColumn(col_name, 
+                                                   when(col("ns3_stopname") == element, lit(col_value)).otherwise(col(col_name)))
 
-# Sélection des colonnes nécessaires
-final_df = merged_df.select(
-    metro_station_df["*"],
-    perimetre_df.drop("name_line").drop("nom_split").drop("line").columns,
-    emplacement_stations_idf_df["longitude"],
-    emplacement_stations_idf_df["latitude"]
-)
+    except Exception as e:
+        print(f"Error processing element {element}: {e}")
 
-# Enregistrement dans Elasticsearch
-final_df.write.format("es").save("clean_perimetre")
+
+def get_last_message_with_key():
+    broker = 'kafka1:29092'  # Adresse du broker Kafka
+    group = 'metro-group'       # ID du groupe de consommateurs
+    topic = 'infosRealTime'  # Topic Kafka à consommer
+    key = 'STIF:StopPoint:Q:22061'           # Clé à filtrer
+
+    # Configuration pour le consommateur Kafka
+    conf = {
+        'bootstrap.servers': broker,
+        'group.id': group,
+        'auto.offset.reset': 'latest'  # Pour obtenir les derniers messages
+    }
+    
+    # Création de l'instance du consommateur
+    consumer = Consumer(conf)
+    
+    # S'abonner au topic
+    consumer.subscribe([topic])
+    
+    while True:
+        # Poll pour un message
+        msg = consumer.poll(timeout=1.0)
+        print(msg)
+        
+        # Message correct
+        message_key = msg.key().decode('utf-8') if msg.key() else None
+        message_value = msg.value().decode('utf-8')
+        
+        if message_key == key:
+            message_data = json.loads(message_value)
+            print(f'Received message with key {key}: {message_data}')
+            consumer.close()
+            return message_data
+                
+    
+last_message = get_last_message_with_key()
+if last_message:
+    print(f'Dernier message avec la clé STIF:StopPoint:Q:22061: {last_message}')
+else:
+    print(f'Pas de message trouvé avec la clé STIF:StopPoint:Q:22061')
 
 spark.stop()
