0c28249b8220a162a770428ed4cd689f89a12320
EdouardY <yuedouard@gmail.com>
2024-06-16 15:11:36 +0200

    df to es index

--- jobs/processing.py
+++ jobs/processing.py
@@ -1,9 +1,8 @@
 from elasticsearch import Elasticsearch
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, lit, when
 from elasticsearch.helpers import scan
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 import pandas as pd
-from confluent_kafka import Consumer, KafkaException, KafkaError
 import json
 
 # Créer une session Spark
@@ -12,13 +11,17 @@ spark = SparkSession.builder \
     .master("spark://spark-master:7077") \
     .getOrCreate()
 
-# API keys for each index
+# API endpoint and keys for each index
+elasticsearch_endpoint = "https://7f0ef8badf50482b9b0d93ef141e14ec.us-central1.gcp.cloud.es.io:443"
 api_key_metro_station = "TWxtbTdZOEItODFDdXJHT2ptcXI6VkdHWl9kcF9UZmk3ZS05dldMVVV4Zw=="
 api_key_perimeter = "QzFsdkc1QUItODFDdXJHT1FuR2g6ZDNxbWdHVXVSQmFzZlZyYW5qSEw4UQ=="
 api_key_emplacement_stations_idf = "bkZWbjU0OEJKR3c1U0ExQWJMQXc6RVRESFRDOC1UMTJNMm5lc19CMXFnQQ=="
+api_key_clean_index = "NDFrVElaQUItODFDdXJHT1duSFg6LWRfQ2JxeGVROHFvTGNWM0R1ZFM0UQ=="
+
+print("The process begins")
 
 es = Elasticsearch(
-        "https://7f0ef8badf50482b9b0d93ef141e14ec.us-central1.gcp.cloud.es.io:443",
+        elasticsearch_endpoint,
         api_key=api_key_perimeter
     )
 
@@ -46,17 +49,20 @@ def elasticsearch_query(es_client, index, query, field):
     return [hit["_source"] for hit in results]
 
 metro_station_es = Elasticsearch(
-    "https://7f0ef8badf50482b9b0d93ef141e14ec.us-central1.gcp.cloud.es.io:443",
+    elasticsearch_endpoint,
     api_key=api_key_metro_station
 )
 
 emplacement_stations_idf_es = Elasticsearch(
-    "https://7f0ef8badf50482b9b0d93ef141e14ec.us-central1.gcp.cloud.es.io:443",
+    elasticsearch_endpoint,
     api_key=api_key_emplacement_stations_idf
 )
 
 # Extraction des noms d'arrêts de perimeter_df
 stopnames_list = perimeter_df.select('ns3_stopname').distinct().rdd.flatMap(lambda x: x).collect()
+
+print("The process may take several minutes")
+print("step 0/2")
 
 # Batch process Elasticsearch queries
 metro_station_results = {}
@@ -64,7 +70,7 @@ for element in stopnames_list:
     try:
         metro_station_results[element] = elasticsearch_query(metro_station_es, "metro_station", element, "nom")[0]
     except Exception as e:
-        print(f"Error processing element {element}: {e}")
+        pass
 
 # Create Spark DataFrame
 columns = ["ns3_stopname", "maps"] + [str(i) for i in range(1, 10)]
@@ -74,6 +80,8 @@ metro_station_updates = [
 ]
 metro_station_df = spark.createDataFrame(metro_station_updates, columns)
 
+print("step 1/2")
+
 # Batch process Elasticsearch queries for the second table
 emplacement_stations_idf_results = {}
 for element in stopnames_list:
@@ -81,7 +89,7 @@ for element in stopnames_list:
         result = elasticsearch_query(emplacement_stations_idf_es, "emplacement-stations-idf", element, "nom_gares")[0]
         emplacement_stations_idf_results[element] = result
     except Exception as e:
-        print(f"Error processing element {element}: {e}")
+        pass
 
 # Create Spark DataFrame
 columns = ["ns3_stopname", "longitude", "latitude"]
@@ -92,56 +100,34 @@ emplacement_stations_idf_updates = [
 
 emplacement_stations_idf_df = spark.createDataFrame(emplacement_stations_idf_updates, columns)
 
+print("step 2/2")
+
 # Join the updates with the original DataFrame
 perimeter_df = perimeter_df.join(metro_station_df, on="ns3_stopname", how="left") \
                            .join(emplacement_stations_idf_df, on="ns3_stopname", how="left")
 
-# Afficher les lignes contenant des valeurs nulles
-# null_rows_df = perimeter_df.filter(col("maps").isNull() | col("longitude").isNull())
-# null_rows_df.show()
 
-# perimeter_df.show(len(stopnames_list), truncate=False)
+clean_index_es = Elasticsearch(
+    elasticsearch_endpoint,
+    api_key=api_key_clean_index
+)
 
+# Fonction pour exclure les champs avec des valeurs nulles d'une ligne et ajouter des champs supplémentaires
+def exclude_nulls_and_add_fields(row):
+    clean_row = {k: v for k, v in row.asDict().items() if v is not None}
+    clean_row['destination'] = None
+    clean_row['waiting_time'] = None
+    return clean_row
 
-def get_last_message_with_key():
-    broker = 'kafka1:29092'  # Adresse du broker Kafka
-    group = 'metro-group'       # ID du groupe de consommateurs
-    topic = 'infosRealTime'  # Topic Kafka à consommer
-    key = 'STIF:StopPoint:Q:22061'           # Clé à filtrer
+# Appliquer la fonction pour exclure les valeurs nulles de chaque ligne et ajouter les nouveaux champs
+rdd = perimeter_df.rdd.map(exclude_nulls_and_add_fields)
 
-    # Configuration pour le consommateur Kafka
-    conf = {
-        'bootstrap.servers': broker,
-        'group.id': group,
-        'auto.offset.reset': 'latest'  # Pour obtenir les derniers messages
-    }
-    
-    # Création de l'instance du consommateur
-    consumer = Consumer(conf)
-    
-    # S'abonner au topic
-    consumer.subscribe([topic])
-    
-    while True:
-        # Poll pour un message
-        msg = consumer.poll(timeout=1.0)
-        print(msg)
-        
-        # Message correct
-        message_key = msg.key().decode('utf-8') if msg.key() else None
-        message_value = msg.value().decode('utf-8')
-        
-        if message_key == key:
-            message_data = json.loads(message_value)
-            print(f'Received message with key {key}: {message_data}')
-            consumer.close()
-            return message_data
-                
-    
-last_message = get_last_message_with_key()
-if last_message:
-    print(f'Dernier message avec la clé STIF:StopPoint:Q:22061: {last_message}')
-else:
-    print(f'Pas de message trouvé avec la clé STIF:StopPoint:Q:22061')
+print("Writing to clean_index in elasticsearch")
+print("This may take a few minutes")
+# Convertir chaque ligne nettoyée en JSON et envoyer à Elasticsearch
+for clean_document in rdd.collect():
+    clean_index_es.index(index="clean_index", body=json.dumps(clean_document))
+
+print("End of the process")
 
 spark.stop()
